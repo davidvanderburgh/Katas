a4f794261ff768975b2ef54c54fe2d1433f2b136
David Vanderburgh <david.a.vanderburgh@gmail.com>
2019-04-07 20:29:28 -0400

    stubbed answers test

--- WordSearch/WordSearch.cs
+++ WordSearch/WordSearch.cs
@@ -8,10 +8,21 @@ namespace WordSearchKata
     {
         public string GridString { get; private set; }
         public char[,] GridArray { get; private set; }
+        public int GridSize { get; private set; }
+        public List<string> Answers { get; private set; }
+
 
         public WordSearch(string gridString)
         {
             GridString = gridString;
+            InitializeGrid();
+        }
+
+        public void InitializeGrid()
+        {
+            string[] rows = GridString.Split("\n");
+
+            
         }
 
 
