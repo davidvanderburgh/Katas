3d5dbefbd0f9a507ef753e37fbbd64d9c4f296ff
David Vanderburgh <david.a.vanderburgh@gmail.com>
2019-04-07 20:09:49 -0400

    added first test from sample kata word search to stub out word search class

---
+++ WordSearch/WordSearch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WordSearchKata
+{
+    public class WordSearch
+    {
+        public string Grid { get; set; }
+
+        public WordSearch(string inputGrid)
+        {
+            Grid = inputGrid;
+        }
+
+        public string GetAnswerString()
+        {
+            return "";
+        }
+
+    }
+}
